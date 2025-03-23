163e0a5bd6ece86ae4da765fe70488385c160b5d
kimmo1019 <liuqiao@buaa.edu.cn>
2025-03-23 03:46:58 -0700

    add BayesGM with low rank structure

--- src/bayesgm/models/__init__.py
+++ src/bayesgm/models/__init__.py
@@ -1,6 +1,6 @@
 from .base import BaseFullyConnectedNet, BayesianFullyConnectedNet, Discriminator
 from .causalbgm import CausalBGM
-from .bayesgm import BayesGM
+from .bayesgm import BayesGM, BayesGM_v2
 
 
-__all__ = ["CausalBGM","BayesGM"]
+__all__ = ["CausalBGM","BayesGM","BayesGM_v2"]
