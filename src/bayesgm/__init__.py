fe35711b11f1e7425e2a4bafa6aaf00e2463d32b
kimmo1019 <liuqiao@buaa.edu.cn>
2025-09-20 10:23:10 -0700

    v0.3.0;add new models: iCausalBGM, CausalBGM_MCMC

--- src/bayesgm/__init__.py
+++ src/bayesgm/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 from .models.causalbgm import CausalBGM, iCausalBGM, CausalBGM_MCMC
 from .datasets import Base_sampler, Sim_Hirano_Imbens_sampler, Sim_Sun_sampler, Sim_Colangelo_sampler, Semi_Twins_sampler, Semi_acic_sampler
